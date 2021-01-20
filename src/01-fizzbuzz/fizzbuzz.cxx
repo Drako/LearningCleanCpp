bb3d63a76353b4f9fbc3e553d194731892ae4e4d
Felix Bytow <drako@drako.guru>
2021-01-20 02:56:10 +0100

    :sparkles: implemented fizzbuzz exercise

--- src/01-fizzbuzz/fizzbuzz.cxx
+++ src/01-fizzbuzz/fizzbuzz.cxx
@@ -1,5 +1,18 @@
 #include "fizzbuzz.hxx"
 
-std::string fizzbuzz(unsigned n) {
-    return "";
+namespace {
+    using namespace std::literals::string_literals;
+
+    std::string fizz(unsigned const n) {
+        return n % 3u ? ""s : "Fizz"s;
+    }
+
+    std::string buzz(unsigned const n) {
+        return n % 5u ? ""s : "Buzz"s;
+    }
+}
+
+std::string fizzbuzz(unsigned const n) {
+    auto const fb = fizz(n) + buzz(n);
+    return fb.empty() ? std::to_string(n) : fb;
 }
--- src/01-fizzbuzz/main.cxx
+++ src/01-fizzbuzz/main.cxx
@@ -3,5 +3,7 @@
 #include "fizzbuzz.hxx"
 
 int main() {
+    for (auto n = 1u; n <= 100u; ++n)
+        std::cout << fizzbuzz(n) << std::endl;
     return 0;
 }
