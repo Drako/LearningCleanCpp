105296561a7bb19b32b0fd058cba0f6b67174dbd
Felix Bytow <drako@drako.guru>
2021-01-22 07:29:15 +0100

    :sparkles: added point solution

--- src/03-point/point.hxx
+++ src/03-point/point.hxx
@@ -3,6 +3,73 @@
 #ifndef POINT_HXX
 #define POINT_HXX
 
-// TODO: add point
+#include <cctype>
+#include <ostream>
+#include <exception>
+
+struct division_by_zero : std::runtime_error {
+    inline division_by_zero(std::string const & message)
+        : std::runtime_error{message}
+    {}
+};
+
+struct Point final {
+
+    int x{};
+    int y{};
+
+    constexpr Point() = default;
+
+    constexpr Point(int x, int y) : x{ x }, y{ y } {}
+
+    constexpr explicit Point(int value) : x{value}, y{value} {}
+
+    constexpr bool is_equal(Point const& p) const {
+        return x == p.x && y == p.y;
+    }
+
+    constexpr Point& operator = (Point const& rhs) {
+        x = rhs.x;
+        y = rhs.y;
+        return *this;
+    }
+};
+
+template <typename Comparable>
+constexpr bool operator == (Comparable const & lhs, Comparable const& rhs) {
+    return lhs.is_equal(rhs);
+}
+
+template <typename Comparable>
+constexpr bool operator != (Comparable const& lhs, Comparable const& rhs) {
+    return !lhs.is_equal(rhs);
+}
+
+constexpr Point operator + (Point const& lhs, Point const& rhs) {
+    return {lhs.x + rhs.x, lhs.y + rhs.y};
+}
+
+constexpr Point operator / (Point const& lhs, int divisor) {
+    if (divisor == 0) {
+        throw division_by_zero{ "divisor must not be 0" };
+    }
+    return { lhs.x / divisor, lhs.y / divisor };
+}
+
+constexpr Point& operator += (Point& lhs, Point const& rhs) {
+    return lhs = lhs + rhs;
+}
+
+constexpr Point& operator ++(Point& self) {
+    ++self.x;
+    ++self.y;
+    return self;
+}
+
+constexpr Point operator++(Point& self, int) {
+    auto const old = self;
+    ++self;
+    return old;
+}
 
 #endif // POINT_HXX
--- src/03-point/test.cxx
+++ src/03-point/test.cxx
@@ -1,3 +1,72 @@
 #include <gtest/gtest.h>
 
+#include <array>
+
 #include "point.hxx"
+
+TEST(PointTest, canCreatePoint) {
+    std::array<Point, 3u> const points{
+        Point{},
+        Point{0, 0},
+        Point{0},
+    };
+
+    Point const expected{0, 0};
+
+    for (auto const& p : points) {
+        EXPECT_EQ(p, expected);
+    }
+}
+
+TEST(PointTest, canAddPoints) {
+    Point const p1{ 10, 12 };
+    Point const p2{ 13, 30 };
+
+    auto const sum = p1 + p2;
+    auto const expected = Point{ 23, 42 };
+    EXPECT_EQ(sum, expected);
+}
+
+TEST(PointTest, canAddPointsModifying) {
+    Point const p1{ 10, 12 };
+    Point const p2{ 13, 30 };
+
+    auto sum = p1;
+    sum += p2;
+    auto const expected = Point{ 23, 42 };
+    EXPECT_EQ(sum, expected);
+}
+
+TEST(PointTest, canPreIncrementPoint) {
+    Point p{ 10, 10 };
+    Point const p2 = ++p;
+
+    Point const expected{ 11, 11 };
+
+    EXPECT_EQ(p, expected);
+    EXPECT_EQ(p2, expected);
+}
+
+TEST(PointTest, canPostIncrementPoint) {
+    Point p{ 10, 10 };
+    Point const p2 = p++;
+
+    Point const expected{ 11, 11 };
+
+    EXPECT_EQ(p, expected);
+    EXPECT_NE(p2, expected);
+}
+
+TEST(PointTest, canDownScalePoint) {
+    static_assert(
+        Point{ 10, 10 } / 5 == Point{ 2, 2 },
+        "can divide at compile time"
+    );
+
+    Point const p{ 10, 10 };
+    Point const actual = p / 5;
+
+    Point const expected{ 2, 2 };
+
+    EXPECT_EQ(actual, expected);
+}
